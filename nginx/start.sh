55805c21bd1e5df7053126b34b2ccf8bfd4a8146
evan <evanhugues@proton.me>
2025-05-22 08:03:55 +0200

    working on nginx issue with reverse server

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-envsubst '$FLASK_SERVER_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+
+if [ -z "$FLASK_SERVER_ADDR" ]; then
+    echo "Erreur: FLASK_SERVER_ADDR n'est pas définie"
+    exit 1
+fi
+
+echo "Configuration nginx avec FLASK_SERVER_ADDR=$FLASK_SERVER_ADDR"
+
+envsubst '$FLASK_SERVER_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf
+
+nginx -t
+
+if [ $? -eq 0 ]; then
+    echo "Configuration nginx valide"
+    exec nginx -g 'daemon off;'
+else
+    echo "Erreur dans la configuration nginx"
+    exit 1
+fi

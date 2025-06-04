c0cbcd4eaaab4d8e8796f215fd76b02359717868
evan <evanhugues@proton.me>
2025-06-04 08:57:13 +0200

    fix: update Docker entrypoint to improve migration handling and increase PostgreSQL readiness wait time

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,15 +2,28 @@
 set -e 
 echo "⏳ Waiting for PostgreSQL to be ready..."
 while ! nc -z val-db 5432; do
-  sleep 1
+  sleep 5
 done
 echo "✅ PostgreSQL is ready!"
+sleep 2
 
-echo "🔧 Initializing Flask-Migrate..."
-flask db init
+# check if db already init 
+# start the compose without migrations 
+if [ ! -d "/app/migrations" ]; then
+  echo "❎ No migrations found, starting the first process..."
+  echo "🔧 Initializing Flask-Migrate..."
+  flask db init
+  echo "🏎️ Create migration..."
+  flask db migrate
+  echo "⬆️ Applying database upgrade..."
+  flask db upgrade
+  echo "🚀 Starting application..."
+  exec "$@"
+else 
+  echo "✅ Migrations directory already exists, skipping initialization."
+  echo "🚀 Starting application..."
+  exec "$@"
+fi
 
-echo "⬆️  Applying database upgrade..."
-flask db upgrade
 
-echo "🚀 Starting application..."
-exec "$@"
+
